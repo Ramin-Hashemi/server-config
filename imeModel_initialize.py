dc0690bc8af9e5e49781ac849fca1f5421637396
Ramin Hashemi <ramin.hashemi@usa.com>
2024-10-11 04:31:29 +0330

    update serverConfig.py packages & imeModel_initialze.py

--- imeModel_initialize.py
+++ imeModel_initialize.py
@@ -4,6 +4,7 @@
 
 
 from serverConfig import create_conn
+import ollama
 
 
 ######################
@@ -16,10 +17,48 @@ from serverConfig import create_conn
 #    It is used to create a model from a Modelfile.
 
 def _initializer(conn):
-    conn.sudo('ollama run llama3.2')
+    conn.sudo('ollama pull llama3.2')
     conn.sudo('ollama create iME -f ./Modelfile')
     conn.sudo('fastapi dev main.py')
 
 
 def initializer(**kwargs):
     _initializer(create_conn())
+
+
+def main(tasks):
+    if len(tasks) <= 1:
+        print('No task name found')
+        return
+    i = 1
+    while i < len(tasks):
+        try:
+            fn = getattr(sys.modules[__name__], tasks[i])
+        except AttributeError:
+            print(f'Cannot find task {tasks[i]}. Quit.')
+            return
+        params = {}
+        j = i + 1
+        while j < len(tasks) and '=' in tasks[j]:
+            k, v = tasks[j].split('=')
+            params[k] = v
+            j += 1
+        i = j
+        print(f'Function is {fn}')
+        print(f'args are {params}')
+        fn(**params)
+
+
+if __name__ == '__main__':
+    '''
+    Run it with
+    $ python main <task1> <key1-task1>=<value1-task1> <key2-task1>=<value2-task2> <task2> <key1-task2>=<value1-task2>
+    E.g.
+    $ python main create_vm
+    
+    Or
+    $ python main pull_repo branch=develop
+    '''
+    import sys
+    tasks = sys.argv
+    main(tasks)
