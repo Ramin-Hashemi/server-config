4877c8f928bfdbffdfcb3000c1e8f6cbaae9b13c
Ramin Hashemi <ramin.hashemi@usa.com>
2024-10-15 23:53:28 +0330

    minor updates

--- server_config.py
+++ server_config.py
@@ -166,10 +166,8 @@ WORKER_CLASS=uvicorn.workers.UvicornWorker
 VENV=$DIR/.venv/bin/activate
 BIND=unix:$DIR/run/gunicorn.sock
 LOG_LEVEL=error
-
 cd $DIR
 source $VENV
-
 exec gunicorn main:app \
 --name $NAME \
 --workers $WORKERS \
