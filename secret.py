7b8695e7763dd797635fffb9d63a6c830583be99
Ramin Hashemi <ramin.hashemi@usa.com>
2024-10-17 02:35:58 +0330

    minor updates

--- secret.py
+++ secret.py
@@ -7,7 +7,7 @@ REMOTE_HOST = '00.00.00.00'
 REMOTE_USER = '****'
 REMOTE_PASSWORD = '*******'
 SSH_KEY_PATH = '~/.ssh/'
-Public_SSH_key = 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAARZpILxXscKBGb89Qh/3rtB08wz2qHux35EnyXihiL ramin.hashemi.myself@gmail.com'
+Public_SSH_key = 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKZHhypBTRWigDhy2vt09oDHS2jbsVaAXT9QeGTZOIQe ramin.hashemi.myself@gmail.com'
 
 ### Git
 GIT_DIR = '~/ime-ai'
