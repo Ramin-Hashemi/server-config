fee332cfbff7f22d3832f3226bdd18e173a0c791
Ramin Hashemi <ramin.hashemi@usa.com>
2024-10-29 22:26:14 +0330

    minor updates

--- server_config.py
+++ server_config.py
@@ -144,17 +144,16 @@ def create_admin_user():
 
 def docker_repository():
     # Set up Docker's apt repository
-    command = """
-    su - root -c '
-    apt-get update &&
-    apt-get install -y ca-certificates curl &&
-    install -m 0755 -d /etc/apt/keyrings &&
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &&
-    chmod a+r /etc/apt/keyrings/docker.asc &&
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null &&
-    apt-get update
-    '
-    """
+    command = [
+        "su", "-", "root", "-c",
+        "apt-get update && "
+        "apt-get install -y ca-certificates curl && "
+        "install -m 0755 -d /etc/apt/keyrings && "
+        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && "
+        "chmod a+r /etc/apt/keyrings/docker.asc && "
+        "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable\" | tee /etc/apt/sources.list.d/docker.list > /dev/null && "
+        "apt-get update"
+    ]
     try:
         # Execute the command and show progress
         with tqdm(total=100, desc="docker_repository", bar_format="{l_bar}{bar} [ time left: {remaining} ]") as pbar:
