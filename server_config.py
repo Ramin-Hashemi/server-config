952673b9747faa82e194449f2f5b4a2601b0d47f
Ramin Hashemi <ramin.hashemi@usa.com>
2024-10-30 13:46:28 +0330

    minor updates

--- server_config.py
+++ server_config.py
@@ -247,7 +247,7 @@ def gnome_extension():
     try:
         # Execute the command and show progress
         with tqdm(total=100, desc="gnome_extension", bar_format="{l_bar}{bar} [ time left: {remaining} ]") as pbar:
-            result = subprocess.run(command, check=True, capture_output=True, text=True, shell=True)
+            result = subprocess.run(command, check=True, capture_output=True, text=True)
             for _ in range (10):
                 time.sleep(0.1)  # Simulate progress
                 pbar.update(10)
